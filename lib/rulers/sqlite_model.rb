3c1beee798156ba3f0192cd940545fb30daf78e2
Xiong Xiong <xiongx8404@gmail.com>
2017-03-20 23:28:35 -0700

    Finish chapter 7

---
+++ lib/rulers/sqlite_model.rb
@@ -0,0 +1,139 @@
+require 'sqlite3'
+require 'rulers/util'
+
+DB = SQLite3::Database.new "test.db"
+
+module Rulers
+  module Model
+    class SQLite
+      def method_missing(name)
+        if @hash.has_key? "#{name}"
+          class << self.class
+            # TODO don't know how to reopen class and define_method dynamically
+            define_method :title do
+              self["title"]
+            end
+          end
+          send name
+        end
+      end
+
+      def method_missing(name)
+        if @hash.has_key? "#{name}"
+          self.class.send(:define_method, name) do
+            self[name]
+          end
+          send name
+        end
+      end
+
+      def method_missing(name)
+        if @hash.has_key? "#{name}"
+          self.class.class_eval <<DEFINE
+            def #{name}
+              self["#{name}"]
+            end
+DEFINE
+          send name
+        end
+      end
+
+      def method_missing(name)
+        if @hash.has_key? "#{name}"
+          lmd = ->() { @hash[name.to_s]}
+          self.class.send(:define_method, name, lmd)
+          send name
+        end
+      end
+
+      def save!
+        unless @hash["id"]
+          new_self = self.class.create @hash
+          @hash = new_self.instance_variable_get :@hash
+          return true
+        end
+
+        fields = @hash.map do |k, v|
+          "#{k}=#{self.class.to_sql(v)}"
+        end.join ","
+
+        DB.execute <<SQL
+UPDATE #{self.class.table}
+SET #{fields}
+WHERE id = #{@hash["id"]}
+SQL
+        true
+      end
+
+      def save
+        self.save! rescue false
+      end
+
+      def self.table
+        Rulers.to_underscore name
+      end
+
+      def self.find(id)
+        row = DB.execute <<SQL
+select #{schema.keys.join ","} from #{table}
+where id = #{id};
+SQL
+        data = Hash[schema.keys.zip row[0]]
+        self.new data
+      end
+
+      def [](name)
+        @hash[name.to_s]
+      end
+
+      def []=(name, value)
+        @hash[name.to_s] = value
+      end
+
+      def self.schema
+        return @schema if @schema
+        @schema = {}
+        DB.table_info(table) do |row|
+          @schema[row["name"]] = row["type"]
+        end
+        @schema
+      end
+
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
+      def self.create(values)
+        values.delete "id"
+        keys = schema.keys - ['id']
+        vals = keys.map do |key|
+          values[key] ? to_sql(values[key]) : "null"
+        end
+        DB.execute <<SQL
+INSERT INTO #{table} (#{keys.join ","}) VALUES (#{vals.join ","});
+SQL
+        data = Hash[keys.zip vals]
+        sql = "SELECT last_insert_rowid();"
+        data["id"] = DB.execute(sql)[0][0]
+        self.new data
+      end
+
+      def self.count
+        DB.execute(<<SQL)[0][0]
+SELECT count(*) FROM #{table}
+SQL
+      end
+    end
+  end
+end
